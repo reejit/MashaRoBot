170b9b3094e8af1fd07ae9e26af49c3b38bfcb44
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-03 12:37:25 +0530

    Update admin.py

--- MashaRoBot/modules/admin.py
+++ MashaRoBot/modules/admin.py
@@ -88,7 +88,7 @@ def promote(update: Update, context: CallbackContext) -> str:
             can_invite_users=bot_member.can_invite_users,
             # can_promote_members=bot_member.can_promote_members,
             can_restrict_members=bot_member.can_restrict_members,
-            can_pin_messages=bot_member.can_pin_messages,
+            
         )
     except BadRequest as err:
         if err.message == "User_not_mutual_contact":
